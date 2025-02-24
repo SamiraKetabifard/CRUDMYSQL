e1ae5fa25d7d44d622ca0d77bcc8eb9e036db4e7
SamiraKetabifard <samiraketabi.fard@gmail.com>
2025-02-24 19:24:09 +0330

    Fixed delete user endpoint and updated repository

--- src/main/java/com/example/crudmysql/controller/UserController.java
+++ src/main/java/com/example/crudmysql/controller/UserController.java
@@ -13,44 +13,40 @@ public class UserController {
 
     @Autowired
     private UserRepository userRepository;
-
     //get all users
     @GetMapping
     public List<User> getAllUsers(){
         return this.userRepository.findAll();
     }
-
     //get user by id
     @GetMapping("/{id}")
-    public User getUserById(@PathVariable(value = "id") long  userId) {
+    public User getUserById(@PathVariable("id") long  userId) {
         return this.userRepository.findById(userId)
-                .orElseThrow(()-> new ResourceNotFoundException("User Not Found With ID :" + userId));
+                .orElseThrow(()-> new ResourceNotFoundException("User Not Found With ID :"
+                        + userId));
     }
-
     //create user
-    @PutMapping
+    @PostMapping
     public User  createUser(@RequestBody User user) {
         return this.userRepository.save(user);
     }
-
     //update user
     @PutMapping("/{id}")
     public User updateUser(@RequestBody User user,@PathVariable("id") long userId) {
         User existingUser = this.userRepository.findById(userId)
-                .orElseThrow(()-> new ResourceNotFoundException("User Not Found With ID :" + userId));
+                .orElseThrow(()-> new ResourceNotFoundException("User Not Found With ID :"
+                        + userId));
         existingUser.setFirstName(user.getFirstName());
         existingUser.setLastName(user.getLastName());
         existingUser.setEmail(user.getEmail());
         return this.userRepository.save(existingUser);
     }
-
     //delete user by id
     @DeleteMapping("/{id}")
     public ResponseEntity<User> deleteUser(@PathVariable("id") long userId){
         User existingUser = this.userRepository.findById(userId)
                 .orElseThrow(()-> new ResourceNotFoundException("User Not Found With ID :" + userId));
         this.userRepository.delete(existingUser);
-        return ResponseEntity.ok().build();    }
-
-
+        return ResponseEntity.ok().build();
+    }
 }
